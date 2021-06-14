ed715c12a6052c40dd742825e7f73606d1c78d7b
kongsubin <qls0919@naver.com>
2021-06-14 18:54:33 +0900

    투표 결과 받아서 보여주기

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 
 class ResultTile extends StatefulWidget{
   ResultTile(this._restaurant, this.idx);
-  final Item _restaurant;
+  final Result _restaurant;
   final int idx;
 
   @override
@@ -20,15 +20,15 @@ class _ResultTileState extends State<ResultTile> {
       leading: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20),
           child: Text((widget.idx + 1).toString(), style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold, color: Colors.black45),)),
-      title: Text(widget._restaurant.restaurant.name, style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold, color: Colors.black45),),
-      trailing: Text('${widget._restaurant.restaurant.place}', style: TextStyle(fontSize: 16, color: Colors.black45),),
+      title: Text(widget._restaurant.name, style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold, color: Colors.black45),),
+      trailing: Text('${widget._restaurant.result}', style: TextStyle(fontSize: 16, color: Colors.black45),),
     );
   }
 }
 
 class TopResultTile extends StatefulWidget{
   TopResultTile(this._item, this.idx);
-  final Item _item;
+  final Result _item;
   final int idx;
   String img = 'assets/1_dotori.png';
 
@@ -71,16 +71,16 @@ class _TopResultTileState extends State<TopResultTile> {
             width: 50,
             height: widget.idx == 0 ? 100 : 40 ,
         ),
-        title: Text(widget._item.restaurant.name, style: TextStyle(fontSize: widget.idx == 0 ? 20 : 18,fontWeight: FontWeight.bold, color: Colors.deepPurpleAccent),),
-        trailing: Text('${widget._item.restaurant.place}', style: TextStyle(fontSize: widget.idx == 0 ? 20 : 18, color: Colors.deepPurpleAccent),),
+        title: Text(widget._item.name, style: TextStyle(fontSize: widget.idx == 0 ? 20 : 18,fontWeight: FontWeight.bold, color: Colors.deepPurpleAccent),),
+        trailing: Text('${widget._item.result}', style: TextStyle(fontSize: widget.idx == 0 ? 20 : 18, color: Colors.deepPurpleAccent),),
       ),
     );
   }
 }
 
 class ResultPage extends StatelessWidget{
-  ResultPage(this._restaurant);
-  final List<Item> _restaurant;
+  ResultPage(this.id);
+  final String id;
 
   @override
   Widget build(BuildContext context) {
@@ -107,7 +107,16 @@ class ResultPage extends StatelessWidget{
                   width: 180,
                 ),
                 SizedBox(height: 40,),
-                ResultBuilder(_restaurant),
+                FutureBuilder<List<Result>>(
+                  future: fetchResult(http.Client(), id),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) print(snapshot.error);
+
+                    return snapshot.hasData
+                        ? ResultBuilder(snapshot.data)
+                        : Center(child: CircularProgressIndicator());
+                  },
+                ),
               ],
             ),
           ),
@@ -120,7 +129,9 @@ class ResultPage extends StatelessWidget{
 
 class ResultBuilder extends StatelessWidget {
   ResultBuilder(this._restaurant);
-  final List<Item> _restaurant;
+  final List<Result> _restaurant;
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -133,7 +144,9 @@ class ResultBuilder extends StatelessWidget {
       separatorBuilder: (BuildContext context, int index){
         return Container(
             height: 20,
-            child: Divider(height: 5));},
+            child: Divider(height: 5));
+
+        },
       itemBuilder: (BuildContext context, int index) {
         return index > 2 ? ResultTile(_restaurant[index], index) : TopResultTile(_restaurant[index], index);
       },
@@ -151,7 +164,7 @@ List<Result> parseRestaurants(String responseBody) {
 }
 
 
-Future<List<Result>> fetchRestaurants(http.Client client, String id) async {
+Future<List<Result>> fetchResult(http.Client client, String id) async {
   final response = await client
       .get(Uri.parse('http://127.0.0.1:5000/vote/result/' + id));
   // Use the compute function to run parsePhotos in a separate isolate.
@@ -160,23 +173,23 @@ Future<List<Result>> fetchRestaurants(http.Client client, String id) async {
 
 
 class Result {
-  final int participantNum;
-  final int ranking;
-  final String category;
-  final String name;
+  int participantNum;
+  double result;
+  // final String category;
+  String name;
 
   Result({
     this.participantNum,
-    this.ranking,
-    this.category,
+    this.result,
+    // this.category,
     this.name,
   });
 
   factory Result.fromJson(dynamic json) {
     return Result(
       participantNum: json['number_of_participant'] as int,
-      ranking: json['ranking_수정필요'] as int,
-      category: json['restaurant_category'] as String,
+      result: json['result'] as double,
+      // category: json['restaurant_category'] as String,
       name: json['restaurant_name'] as String,
     );
   }
