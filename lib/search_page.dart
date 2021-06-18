03f40458429ded0435d6821d65c69fa23884a201
kongsubin <qls0919@naver.com>
2021-06-18 21:34:21 +0900

    서치 기능 추가

---
+++ lib/search_page.dart
@@ -0,0 +1,127 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mukvote_web/search_result_page.dart';
+
+class SearchPage extends StatefulWidget {
+  static String tag = 'contactlist-page';
+
+  @override  State<StatefulWidget> createState() {
+    return new _SearchPage();
+  }
+}
+
+class _SearchPage extends State<SearchPage> {
+  TextEditingController searchController = new TextEditingController();
+  String filter;
+
+  @override  initState() {
+    searchController.addListener(() {
+      setState(() {
+        filter = searchController.text;
+      });
+    });
+  }
+
+  @override  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            leading: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(
+                Icons.arrow_back_ios_outlined,
+                color: Colors.deepPurpleAccent, // add custom icons also
+              ),
+            ),
+          // title: Text('먹VOTE',
+          //     style: TextStyle(
+          //       color: Colors.deepPurpleAccent,
+          //       fontWeight: FontWeight.bold,
+          //     )),
+          backgroundColor: Colors.white,),
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 50.0),
+            children: [
+              SizedBox(height: 80),
+              Text('음식점 찾기',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.deepPurpleAccent,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(height: 10),
+              Image.asset(
+                'assets/login.png',
+                height: 150,
+              ),
+              SizedBox(height: 40),
+              Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Row(children: <Widget>[
+                    Expanded(
+                        child: TextField(
+                          controller: searchController,
+                          decoration:
+
+                          InputDecoration(
+                            contentPadding: EdgeInsets.all(8),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                              new BorderRadius.circular(32.0),
+                              borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+                            ),
+                            border: new OutlineInputBorder(
+                              borderRadius:
+                              new BorderRadius.circular(32.0),
+                              borderSide: new BorderSide(color: Colors.deepPurpleAccent),
+                            ),
+                            // prefixIcon: Icon(Icons.search),
+                          ),
+                        )),
+                  ])),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child:
+                  Container(
+                    height: 40.0,
+                    width: 120.0,
+                    child: FittedBox(
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          Navigator.push( context, MaterialPageRoute(
+                              builder: (context) => SearchResultPage(keyword: searchController.text)));
+
+                        },
+                        materialTapTargetSize: MaterialTapTargetSize.padded,
+                        backgroundColor: Colors.deepPurpleAccent,
+                        icon: Icon(Icons.search, size: 18,),
+                        label: Text("검색하기", style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        );
+  }
+}
+
---
+++ lib/search_result_page.dart
@@ -0,0 +1,146 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class SearchResultPage extends StatefulWidget {
+  final String keyword;
+
+  SearchResultPage({Key key, this.keyword}) : super(key: key);
+
+  @override
+  _SearchResultPage createState() => _SearchResultPage(keyword);
+
+}
+
+class _SearchResultPage extends State<SearchResultPage> {
+  String keyword;
+
+  _SearchResultPage(this.keyword);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back_ios_outlined,
+            color: Colors.deepPurpleAccent, // add custom icons also
+          ),
+        ),
+       title: Text('검색결과 ' + keyword,
+            style: TextStyle(
+              color: Colors.deepPurpleAccent,
+              fontWeight: FontWeight.bold,
+            )),
+        backgroundColor: Colors.white,),
+      body: FutureBuilder<List<Restaurant>>(
+          future: fetchRestaurants(http.Client(), keyword),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+
+            return snapshot.hasData
+                ? ListView(children: [ListkBuilder(snapshot.data)])
+                : Center(child: CircularProgressIndicator());
+          }),
+    );
+  }
+}
+
+class ListkBuilder extends StatelessWidget {
+  ListkBuilder(this.restaurantItems);
+
+  var restaurantItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      physics: ClampingScrollPhysics(),
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(8),
+      // itemCount: items.length + 1,
+      itemCount: restaurantItems.length,
+      separatorBuilder: (BuildContext context, int index) {
+        return Container(height: 20, child: Divider(height: 5));
+      },
+      itemBuilder: (BuildContext context, int index) {
+        // return ShowRestaurant(restaurantItems[index]);
+        return ListTile(
+            title: Text(
+              restaurantItems[index].name,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurpleAccent),
+            ),
+            trailing: Text(
+              restaurantItems[index].category,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.deepPurpleAccent,
+              ),
+            ));
+      },
+    );
+  }
+}
+
+// For connection server
+// A function that converts a response body into a List<Photo>.
+List<Restaurant> parseRestaurants(String responseBody) {
+  var restaurantJson = jsonDecode(responseBody)['poll_data'] as List;
+  List restaurants =
+      restaurantJson.map((tagJson) => Restaurant.fromJson(tagJson)).toList();
+  return restaurants;
+}
+
+Future<List<Restaurant>> fetchRestaurants(
+    http.Client client, String place) async {
+  final response =
+      await client.get(Uri.parse('http://127.0.0.1:5000/poll/' + '36'));
+  // .get(Uri.parse('http://127.0.0.1:5000/restaurant/' + place));
+  // Use the compute function to run parsePhotos in a separate isolate.
+  return parseRestaurants(response.body);
+}
+
+Future<List<Restaurant>> fetchRestaurants2(
+    http.Client client, String place) async {
+  final response =
+      await client.get(Uri.parse('http://127.0.0.1:5000/poll/' + '40'));
+  // .get(Uri.parse('http://127.0.0.1:5000/restaurant/' + place));
+  // Use the compute function to run parsePhotos in a separate isolate.
+  return parseRestaurants(response.body);
+}
+
+class Restaurant {
+  final int orderCount;
+  final int id;
+  final String category;
+  final String name;
+  final String place;
+  final int priority;
+
+  Restaurant(
+      {this.orderCount,
+      this.id,
+      this.category,
+      this.name,
+      this.place,
+      this.priority});
+
+  factory Restaurant.fromJson(dynamic json) {
+    return Restaurant(
+      orderCount: json['order_count'] as int,
+      id: json['restaurant_id'] as int,
+      category: json['restaurant_category'] as String,
+      name: json['restaurant_name'] as String,
+      place: json['restaurant_place'] as String,
+      priority: json['restaurant_priority'] as int,
+    );
+  }
+}
